4504ec50e4e7649c96a7b28a1dcae6a76de5cc59
swati konnuri <swatikonnuri@gmail.com>
2026-03-12 17:54:12 +0530

    added more examples for aggregate functions

---
+++ src/Explode_Functions/explode_outer.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("ExplodeExample").getOrCreate()
+
+data = [
+    ("Swati", ["Python","SQL","Spark"]),
+    ("Rahul", ["Java","Python"]),
+    ("Anita", None)
+]
+
+df = spark.createDataFrame(data, ["Name","Skills"])
+df.show(truncate=False)
+df.select("Name", explode_outer("Skills")).show()
