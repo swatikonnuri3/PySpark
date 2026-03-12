f75dfd25b66009a3629cf077fae558759692cf61
swati konnuri <swatikonnuri@gmail.com>
2026-03-12 18:51:20 +0530

    created schema using StructType and StructField

---
+++ src/schema/struct.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField,IntegerType,StringType
+spark=SparkSession.builder.appName("structs").getOrCreate()
+data=[\
+    (1,("lakshmanan","palaniappan"),"Dev"), \
+    (2,("fname","lname"),"Tester"),\
+    ]
+name_schema=StructType(
+    [
+        StructField("firstName",StringType()),
+        StructField("lastName",StringType())
+
+    ]
+)
+schema=StructType(
+    [
+        StructField("id",IntegerType(),True),
+        StructField("name",name_schema),
+        StructField("position",StringType())
+    ]
+)
+
+df=spark.createDataFrame(data,schema)
+df.show()
+df.printSchema()
